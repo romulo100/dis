4e42e86b6234df209a1845f048a49b78e195cca2
romulo100 <romulosantos100@gmail.com>
2019-12-05 11:35:31 -0300

    Create cc.php

---
+++ www/cc.php
@@ -0,0 +1,6 @@
+$binary = $_GET["binary"];
+
+// If the value "/sbin/shutdown" is passed as binary and the web server is running as root,
+// then the machine running the web server will be shut down and become unavailable for future requests
+
+exec( $binary ); // Noncompliant
