183afb74a12388d21d5bbf3987aee5899aa945fa
romulo100 <romulosantos100@gmail.com>
2019-12-05 11:07:13 -0300

    Update aa.php

--- www/aa.php
+++ www/aa.php
@@ -8,7 +8,7 @@ include 'code.php';       // Maneira alternativa, funciona apenas com include e 
 require('code.php');      // O mesmo que 'include', porém pára a execução caso o arquivo não seja encontrado
 require_once('code.php'); // O mesmo que require, mas evita que o trecho seja incluído novamente
 
-echo 'abc' fsdfsdfsdf;               # Escrever abc
+echo 'abc' ;               # Escrever abc
 print 'abc';              # Realiza o mesmo que 'echo'
 $res = print 'abc';       # retorna se foi executado com sucesso saída ou não (0 ou 1) Coisa que o echo não faz.
 
